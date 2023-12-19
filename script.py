fd2cab5513ba849c685314d55c6f9bec6667db3e
Khavushka <khavushka@gmail.com>
2023-12-19 11:21:38 +0100

    last commit

---
+++ script.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense, Conv2D, Flatten, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD
+
+import tensorflow_datasets as tfds
